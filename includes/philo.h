4c113e23f94acf6ffc095bc061aa643cb4d66208
v1dusss <vsivanat@student.42heilbronn.de>
2024-07-09 14:36:04 +0200

    initialize forks and philos

--- includes/philo.h
+++ includes/philo.h
@@ -3,21 +3,11 @@
 # define PHILO_H
 
 # include <pthread.h>
+# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
 # include <unistd.h>
-
-typedef struct s_table
-{
-	int				philo_num;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				eat_times;
-	int				dead_count;
-	uint64_t		start_time;
-}					t_table;
 
 typedef struct s_philo
 {
@@ -27,8 +17,21 @@ typedef struct s_philo
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 	pthread_t		thread;
-	t_table			*table;
 }					t_philo;
+typedef struct s_table
+{
+	int				philo_num;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_times;
+	int				dead_count;
+	uint64_t		start_time;
+	pthread_mutex_t	*forks;
+	t_philo			*philo;
+}					t_table;
+
+# define ERROR_PHILO "\033[1;31mError: Failed to create philosophers\033[0m\n"
 
 void				usage(void);
 void				parsing(int argc, char **argv, t_table *table);
--- src/main.c
+++ src/main.c
@@ -1,18 +1,63 @@
 #include "philo.h"
 
+void	print_parsing(t_table *table)
+{
+	printf("philo_num: %d\n", table->philo_num);
+	printf("time_to_die: %d\n", table->time_to_die);
+	printf("time_to_eat: %d\n", table->time_to_eat);
+	printf("time_to_sleep: %d\n", table->time_to_sleep);
+	if (table->eat_times != -1)
+		printf("eat_times: %d\n", table->eat_times);
+	printf("\033[1;32mParsing is done! \033[0m\n");
+}
+
+void	destory_forks(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	while (++i < table->philo_num)
+		pthread_mutex_destroy(&table->forks[i]);
+	free(table->forks);
+	free(table->philo);
+}
+
+bool	create_philo(t_table *table, t_philo *philo)
+{
+	int	i;
+
+	i = -1;
+	table->philo = philo;
+	table->forks = malloc(sizeof(pthread_mutex_t) * table->philo_num);
+	if (!table->forks)
+		return (false);
+	while (++i < table->philo_num)
+		pthread_mutex_init(&table->forks[i], NULL);
+	i = -1;
+	philo = (t_philo *)malloc(sizeof(t_philo) * table->philo_num);
+	if (!philo)
+		return (false);
+	while (++i < table->philo_num)
+	{
+		philo[i].id = i + 1;
+		philo[i].eat_times = 0;
+		philo[i].last_eat = 0;
+		(philo[i].left_fork = &table->forks[i]);
+		(philo[i].right_fork = &table->forks[(i + 1) % table->philo_num]);
+	}
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
 	t_table	table;
+	t_philo	philo;
 
 	if (argc < 5 || argc > 6)
 		usage();
 	parsing(argc, argv, &table);
-	printf("philo_num: %d\n", table.philo_num);
-	printf("time_to_die: %d\n", table.time_to_die);
-	printf("time_to_eat: %d\n", table.time_to_eat);
-	printf("time_to_sleep: %d\n", table.time_to_sleep);
-	if (table.eat_times != -1)
-		printf("eat_times: %d\n", table.eat_times);
-	printf("\033[1;32mParsing is done! \033[0m\n");
+	print_parsing(&table);
+	if (!create_philo(&table, &philo))
+		return (printf(ERROR_PHILO), destory_forks(&table), 1);
 	return (0);
 }
